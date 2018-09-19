93f322da1a376c2fe81c628bfc9567a95a8b1460
lnwhsq <38019642+lnwhsq@users.noreply.github.com>
2018-09-20 09:32:29 +1000

    Add files via upload

--- qirc/template.php
+++ qirc/template.php
@@ -16,15 +16,14 @@
  *   The name of the template being rendered ("maintenance_page" in this case.)
  */
 function qirc_menu_tree__main_menu($variables) {
-  # return '<ul class="navbar-nav mr-auto first-sidebar" id="main-menu-links">' . $variables['tree'] . '</ul>';
+
 }
 
 /**
  * Implements hook_js_alter
  */
 function qirc_js_alter(&$javascript) {
-  // Swap out jQuery to use an updated version of the library.
-  //$javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'qirc') . '/js/jquery3.js';
+ 
 }
 
 function qirc_preprocess_page(&$variables){
@@ -35,56 +34,10 @@ function qirc_preprocess_page(&$variables){
 	
 	foreach ($menus as $key => $val){
 			$menustr[$val['link_path']] = '<a class="" href="'.$val['link_path'].'" role="listitem" style="padding-bottom:100vh;"><div><span>'.$val['link_title'].'</span></div></a>';
-			
 	}
 	
 	$variables['custommenulinks'] = $menustr;
-	
-	/*
-	$strOutput;
-
-	if ($variables['node']->type == 'awardscustomcontent') {
-    	$variables['theme_hook_suggestions'][] = 'page__awardscustomcontent';
-  }
-
-
-
-	
-	$variables['filecurrentpath'] = 'Currentpath: '.request_path();
-	#$variables['filelistbynode'] = $dirscan;
-
-	
-	$dir = "public://awards";
-    $filepath = file_scan_directory($dir, '/.*\.pdf$/');
-    $variables['foolist'] = $filepath;
-	
-	 
-	$path = drupal_realpath('public://awards');
-	$dirs = array();
-	// directory handle
-	$dir = dir($path);
-
-	while (false !== ($entry = $dir->read())) {
-		if ($entry != '.' && $entry != '..') {
-		if (is_dir($path . '/' .$entry)) {
-            $dirs[] = $entry; 
-		}
-		else{
-			if($entry != '.htaccess'){
-			 $strOutput .= $entry .'<br>';
-			
-		  }	
-		}
-    }
-}
-		$variables['filescan'] = $strOutput;
-   		# echo '<pre>';print_r($dirs);echo '</pre>';
-	
-	*/
-	
-	
-	
-	
+		
 }
 
 
@@ -96,7 +49,9 @@ function qirc_form_search_api_page_search_form_alter(&$form, &$form_state, $form
 '#title_display' => $compact ? 'invisible' : 'before',
 '#default_value' => $keys,
 '#size' => $compact ? 15 : 30, );
-$form['keys_' . $page->id]['#attributes']['placeholder'] = t('Enter your keywords yo'); }
+$form['keys_' . $page->id]['#attributes']['placeholder'] = t('Enter your keywords yo'); 
+
+}
 
 /**
 * hook_form_FORM_ID_alter
@@ -105,39 +60,17 @@ function qirc_form_search_block_form_alter(&$form, &$form_state, $form_id) {
 	#echo '$form_id'. $form_id;
 	$form['search_block_form']['#attributes']['placeholder'] = t('Search'); 
 	$form['actions']['submit']['#value'] = t(''); // Change the text on the submit button
-	$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/Magnifying glass-01.png','#alt'=>'Search QIRC');
+	$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/Magnifying_glass-01.png','#alt'=>'Search QIRC');
 	$form['search_block_form']['#size'] = 20;  // define size of the textfield
 	$form['actions']['submit']['#attributes'] = array('class' => array('qircclass'));
 	$form['actions']['submit']['#name'] = 'qirc_search';
-	/*
-    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
-    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
-    $form['search_block_form']['#size'] = 30;  // define size of the textfield
-    $form['search_block_form']['#default_value'] = t('Search'); // Set a default value for the textfield
-    $form['actions']['submit']['#value'] = t('Search'); // Change the text on the submit button
-   # $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/search_icon.png');
-    $form['actions']['submit']['#attributes'] = array('class' => 'my_class');
 
-    // Add extra attributes to the text box
-    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
-    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
-    // Prevent user from searching the default text
-    $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
-
-    // Alternative (HTML5) placeholder attribute instead of using the javascript
-    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
-	*/
 }
 
 /** alter custom form **/
 
 function qirc_form_alter(&$form, &$form_state, $form_id){
-	#echo '$form_id'. $form_id;
-	#$form_id search_api_page_search_form 
-	#$form_id search_api_page_search_form_default_search
-	
-	
-	
+		
 	if($form_id == 'search_api_page_search_form_default_search'){
 		$form['search_block_form']['#attributes']['placeholder'] = t(''); 
 	    $form['actions']['submit']['#value'] = t(''); // Change the text on the submit button
@@ -185,7 +118,8 @@ function qirc_file_link($variables) {
 
   $formattedFileSize = round($file->filesize/10000,2);
   
-  return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . ' ('. $file->filemime .'), ' . $formattedFileSize.' KB)</span>';
+  return '<span class="file">' . $icon . ' ' . l($link_text, $url, $options) . '</span>';
+  
 } ///end pdf link
 
 
